085a1b7babcea6090d20f48b6325b71e0eee849f
truonghoangthuan <tthun32@gmail.com>
2021-11-05 22:41:52 +0700

    Validate password for user sign up

--- src/server/service/UserService.java
+++ src/server/service/UserService.java
@@ -4,8 +4,15 @@ import server.dao.UserDao;
 import server.entity.User;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class UserService {
+
+    public static void main(String[] args) {
+        UserService user = new UserService();
+        String password = "123Thuan#";
+        System.out.println(user.isValidPassword(password));
+    }
 
     public String createUser(String usernameText, String passwordText, String role) throws SQLException {
 
@@ -13,7 +20,7 @@ public class UserService {
         User user = new User();
 
         // todo: validate password
-        if(!isValidPassword(passwordText))
+        if (!isValidPassword(passwordText))
             return "Invalid password! Please try again";
 
         user.setUserName(usernameText);
@@ -27,6 +34,18 @@ public class UserService {
 
     // Todo: complete this
     private boolean isValidPassword(String passwordText) {
-        return true;
+        // Use Scanner to validate input password.
+        Scanner sc = new Scanner(passwordText);
+        //^                 # start-of-string
+        //(?=.*[0-9])       # a digit must occur at least once
+        //(?=.*[a-z])       # a lower case letter must occur at least once
+        //(?=.*[A-Z])       # an upper case letter must occur at least once
+        //(?=.*[@#$%^&+=])  # a special character must occur at least once
+        //(?=\S+$)          # no whitespace allowed in the entire string
+        //.{8,}             # anything, at least eight places though
+        //$                 # end-of-string
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        // Return boolean value.
+        return sc.hasNext(regex);
     }
 }
